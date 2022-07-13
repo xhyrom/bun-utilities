1782e1eb609f0629e27317751a05efa8575ea0d8
Hyro <generalkubo@gmail.com>
2022-07-13 21:19:18 +0200

    tests(bun): fix

--- tests/bun/index.spec.mjs
+++ tests/bun/index.spec.mjs
@@ -2,9 +2,5 @@ import { expect, it } from 'bun:test';
 import { exec, spawn } from '../../lib/index.mjs';
 
 it('exec from native', async() => {
-  expect((await exec(['echo', 'test'])).output.replace(/\n|\r/g, '')).toBe('test');
-})
-
-it('spawn from native', async() => {
-  expect((await (await spawn('echo', [ 'test' ])).stdout.replace(/\n|\r/g, ''))).toBe('test');
+  expect((await exec(['echo', 'test'])).stdout.replace(/\n|\r/g, '')).toBe('test');
 })
