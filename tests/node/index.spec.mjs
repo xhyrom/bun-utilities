50091dd20886dc8911e179f718158abd07861408
Hyro <generalkubo@gmail.com>
2022-07-13 21:18:48 +0200

    tests(node): fix

--- tests/node/index.spec.mjs
+++ tests/node/index.spec.mjs
@@ -1,10 +1,6 @@
 import test from 'ava'
-import { exec, spawn } from '../../lib/index.js';
+import { exec } from '../../lib/index.js';
 
 test('exec from native', async(t) => {
-  t.is((await exec(['echo', 'test'])).output.replace(/\n|\r/g, ''), 'test');
-})
-
-test('spawn from native', async(t) => {
-  t.is((await (await spawn('echo', [ 'test' ])).stdout.replace(/\n|\r/g, '')), 'test');
+  t.is((await exec(['echo', 'test'])).stdout.replace(/\n|\r/g, ''), 'test');
 })
