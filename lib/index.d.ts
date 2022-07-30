c23081e5a8c678411e9d0f97eb1e97d6a62d822f
xHyroM <generalkubo@gmail.com>
2022-07-30 08:38:49 +0200

    chore: build

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -18,7 +18,7 @@ export interface Options {
 export function exec(commandWithArgs: Array<string>, options?: Options | undefined | null): { stdout: undefined, stderr: undefined, exitCode?: number, isExecuted: false } | { stdout: string, stderr: string, exitCode?: number, isExecuted: true }
 export function spawn(command: string, args: Array<string>, options?: Options | undefined | null): { stdout: undefined, stderr: undefined, exitCode?: number, isExecuted: false } | { stdout: string, stderr: string, exitCode?: number, isExecuted: true }
 export function execAndDontWait(commandWithArgs: Array<string>, options?: Options | undefined | null): ProcessResult
-export function spawnAndDontWait(command: string, args: Array<string>, options?: Options | undefined | null): ProcessResult
+export function spawnAndDontWait(command: string, args: Array<string>, options?: Options | undefined | null): { isExecuted: true }
 export interface RecrusiveOptions {
   recursive?: boolean
 }
