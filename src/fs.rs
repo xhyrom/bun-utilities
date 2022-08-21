2e32a63a8b527c367a57fa7aa3d315a349097a55
harryrabin <52754948+harryrabin@users.noreply.github.com>
2022-08-20 18:08:24 -0700

    Fixed typo and some tree-of-doom

--- src/fs.rs
+++ src/fs.rs
@@ -1,7 +1,7 @@
-use std::{io, path::Path, fs};
+use std::{fs, io, path::Path};
 
 #[napi(object)]
-pub struct RecrusiveOptions {
+pub struct Recursive {
     pub recursive: Option<bool>,
 }
 
@@ -12,46 +12,39 @@ pub struct CopyDirOptions {
 }
 
 #[napi]
-pub fn rmdir(path: String, options: Option<RecrusiveOptions>) -> String {
-    let options = options.unwrap_or(RecrusiveOptions {
-        recursive: Some(false)
+pub fn rmdir(path: String, options: Option<Recursive>) -> String {
+    let options = options.unwrap_or(Recursive {
+        recursive: Some(false),
     });
 
     let recursive = options.recursive.unwrap_or(false);
 
     let path = Path::new(&path);
     let error;
-    if path.exists() {
-        if recursive {
-            let message = match fs::remove_dir_all(path) {
-                Ok(()) => "ok",
-                Err(e) => {
-                    error = format!("{}", e.kind().to_string());
-                    &error
-                },
-            };
-
-            return message.to_string();
-        } else {
-            let message = match fs::remove_dir(path) {
-                Ok(()) => "ok",
-                Err(e) => {
-                    error = format!("{}", e.kind().to_string());
-                    &error
-                }
-            };
-
-            return message.to_string();
-        }
-    } else {
-        return "Invalid file".to_string()
+    if !path.exists() {
+        return "Invalid file".to_string();
     }
+
+    let result = match recursive {
+        true => fs::remove_dir_all(path),
+        false => fs::remove_dir(path),
+    };
+
+    let message = match result {
+        Ok(()) => "ok",
+        Err(e) => {
+            error = format!("{}", e.kind().to_string());
+            &error
+        }
+    };
+
+    message.to_string()
 }
 
 #[napi]
-pub fn copyfile(src: String, dest: String, options: Option<RecrusiveOptions>) -> String {
-    let options = options.unwrap_or(RecrusiveOptions {
-        recursive: Some(false)
+pub fn copyfile(src: String, dest: String, options: Option<Recursive>) -> String {
+    let options = options.unwrap_or(Recursive {
+        recursive: Some(false),
     });
 
     let recursive = options.recursive.unwrap_or(false);
@@ -88,7 +81,7 @@ fn __copyfile(src: &Path, dest: &Path) -> io::Result<u64> {
 pub fn copydir(src: String, dest: String, options: Option<CopyDirOptions>) -> String {
     let options = options.unwrap_or(CopyDirOptions {
         recursive: Some(false),
-        copy_files: Some(true)
+        copy_files: Some(true),
     });
 
     let recursive = options.recursive.unwrap_or(false);
@@ -106,22 +99,18 @@ pub fn copydir(src: String, dest: String, options: Option<CopyDirOptions>) -> St
     }
 
     if destination.exists() && recursive {
-        rmdir(destination.to_str().unwrap().to_string(), Some(RecrusiveOptions {
-            recursive: Some(true)
-        }));
+        rmdir(
+            destination.to_str().unwrap().to_string(),
+            Some(Recursive {
+                recursive: Some(true),
+            }),
+        );
     }
 
-    let error;
-    let message = match __copydir(
-        source,
-        destination,
-        recursive,
-        copy_files,
-    ) {
+    let message = match __copydir(source, destination, recursive, copy_files) {
         Ok(..) => "ok",
         Err(e) => {
-            error = format!("{}", e.kind().to_string());
-            &error
+            format!("{}", e.kind().to_string())
         }
     };
 
@@ -129,19 +118,27 @@ pub fn copydir(src: String, dest: String, options: Option<CopyDirOptions>) -> St
 }
 
 // Internal implementation for copydir function
-fn __copydir(src: impl AsRef<Path>, dest: impl AsRef<Path>, recursive: bool, copy_files: bool) -> io::Result<()> {
+fn __copydir(
+    src: impl AsRef<Path>,
+    dest: impl AsRef<Path>,
+    recursive: bool,
+    copy_files: bool,
+) -> io::Result<()> {
     fs::create_dir_all(&dest)?;
     for entry in fs::read_dir(src)? {
         let entry = entry?;
         let ty = entry.file_type()?;
         if ty.is_dir() {
-            __copydir(entry.path().to_str().unwrap().to_string(), dest.as_ref().join(entry.file_name()), recursive, copy_files)?;
+            __copydir(
+                entry.path().to_str().unwrap().to_string(),
+                dest.as_ref().join(entry.file_name()),
+                recursive,
+                copy_files,
+            )?;
         } else if copy_files {
             let dest_path = dest.as_ref().join(entry.file_name());
-            if dest_path.exists() {
-                if recursive {
-                    __copyfile(&entry.path(), &dest_path)?;
-                }
+            if dest_path.exists() && recursive {
+                __copyfile(&entry.path(), &dest_path)?;
             } else {
                 __copyfile(&entry.path(), &dest_path)?;
             }
